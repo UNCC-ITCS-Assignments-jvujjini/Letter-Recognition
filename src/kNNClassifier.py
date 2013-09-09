36271cece1ab0dbb6f8a52b7831f9df7bc25ca5f
Jagan Vujjini <jagan120@gmail.com>
2013-09-08 21:00:17 -0400

    second commit

--- src/kNNClassifier.py
+++ src/kNNClassifier.py
@@ -1,39 +1,65 @@
-import math
+import numpy as np
+#import time
+#from scipy.spatial import KDTree
 
-class kNNClassifier():
+def condense(training_set,condensed_set):  
     
-    def classify(self,training_set,test_set):  
+    result = []
+    check = condensed_set
         
-        result = []  
-        num = 0    
+    for case in training_set:
         
-        for case in test_set:
+        min_distance = float("inf")
+        
+        for example in condensed_set:
             
-            min_diff = float("inf")
+            distance = np.linalg.norm(case[1:]-example[1:])
             
-            for example in training_set:
+            if distance < min_distance:
+                min_distance = distance
+                #min_distance_variable = example[0]
                 
-                diff = self.diff(example,case)
-                
-                if diff < min_diff:
-                    min_diff = diff
-                    min_diff_index = example[0]
-                
-            test_result = min_diff_index
-            num = num + 1
-            print str(num) +":"+ str(test_result)+":"+str(diff)
-            result.append(test_result)
-            
-        return result
-            
-    def diff(self,train,test):
+                if case[0] != example[0]:
+                    array = np.array([case])
+                    np.concatenate([condensed_set,array])
+                    np.delete(training_set,array)
         
-        sigma = 0
-        attributes_length = len(test)
+        print condensed_set
+    
+    while True:
+        if np.array_equal(result, check):
+            break
+        result = condense(training_set,condensed_set)
         
-        for k in xrange(1,attributes_length):
-            sigma += (test[k]-train[k])**2
-        
-        ans = (1/attributes_length-1) * float(math.sqrt(sigma))
-        
-        return ans
+    return result
+
+fname = 'letter-recognition.data'
+
+data = np.loadtxt(fname, np.float32, delimiter=',', converters={ 0 : lambda ch : ord(ch)-ord('A') })
+training_data, test_data, test_response = data[:10,:],data[19990:,:],data[19990:,0]
+
+condensed_data = test_data[:2]
+count = 0
+x = condense(training_data,condensed_data)
+'''for y in x:
+    count +=1'''
+print x.shape()
+
+
+#kdtree = KDTree(training_data,leafsize=10)
+
+#print kdtree.query(training_data, 5, 1, 2)
+
+'''start = time.time()
+result = classify(training_data,test_data)
+print result
+end = time.time()
+print "It took " + str((end - start)/60) + " minutes"
+
+correct = 0
+
+for x in xrange(len(test_response)):
+    if result[x] == test_response[x]:
+        correct += 1
+
+print str(correct) + " test cases classified correctly out of " + str(len(test_response)) + " with an efficiency of " + str(float((correct*100))/len(test_response))''' 
