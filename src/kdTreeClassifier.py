bce6adef4c125e8d1b361808086dbc63b5318542
Jagan Vujjini <jagan120@gmail.com>
2013-09-10 14:15:22 -0400

    change to kdTreeClassifier

--- src/kdTreeClassifier.py
+++ src/kdTreeClassifier.py
@@ -2,6 +2,32 @@ import numpy as np
 import time
 from scipy.spatial import KDTree
 
+def classify(tree,test_set):  
+        
+        result = []
+        
+        for case in test_set:
+            
+            '''min_distance = float("inf")
+            
+            for example in training_set:
+                
+                distance = np.linalg.norm(case[1:]-example[1:])
+                
+                if distance < min_distance:
+                    min_distance = distance
+                    min_distance_variable = example[0]
+            
+            test_result = min_distance_variable    
+            result.append(chr(int(test_result)+65))'''
+            
+            tree_value = tree.query(case,1,1.0,2,10.0)
+            point = tree_value[1]                
+
+            result.append(point)
+            
+        return result
+
 fname = 'letter-recognition.data'
 
 data = np.loadtxt(fname, np.float32, delimiter=',', converters={ 0 : lambda ch : ord(ch)-ord('A') })
@@ -9,9 +35,8 @@ training_data, test_data, test_response = data[:15000,:],data[15000:,:],data[150
 kdtree = KDTree(training_data)
 
 start = time.time()
-values = kdtree.query(test_data, 1, 1.0, 2, 10.0)
-
-points = values[1]
+#values = kdtree.query(test_data, 1, 1.0, 2, 10.0)
+points = classify(kdtree,test_data)
 
 result = []
 
